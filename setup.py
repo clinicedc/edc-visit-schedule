16ec32726e3d65f2b9fcb625249344afb4cc99d0
Leslie <leslie.kep@gmail.com>
2015-08-21 15:04:21 +0200

    updated setup

--- setup.py
+++ setup.py
@@ -10,18 +10,18 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='edc-visit-tracking',
+    name='edc-visit-schedule',
     version='0.1.0',
     author=u'Erik van Widenfelt',
     author_email='ew2789@gmail.com',
     packages=find_packages(),
     include_package_data=True,
-    url='https://github.com/erikvw/edc-visit-tracking',
+    url='https://github.com/botswana-harvard/edc-visit-schedule',
     license='GPL license, see LICENCE',
     description='.',
     long_description=README,
     zip_safe=False,
-    keywords='django visit tracking clinical research',
+    keywords='django visit schedule clinical research',
     install_requires=[
         'Django>=1.7',
         'django-extensions>=1.5.5',
