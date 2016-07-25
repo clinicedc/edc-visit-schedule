5cfae84e6d596b7a141817ac04862459319af5b4
Tshepiso Setsiba <tsetsiba@bhp.org.bw>
2016-07-25 19:58:05 +0200

    visit schedule template, not complete

--- edc_visit_schedule/views.py
+++ edc_visit_schedule/views.py
@@ -4,4 +4,4 @@ from django.views.generic.base import TemplateView
 
 class HomeView(EdcBaseViewMixin, TemplateView):
 
-    template_name = 'edc_visit_schedule/home.html'
+    template_name = 'edc_visit_schedule/visit_schedule.html'
