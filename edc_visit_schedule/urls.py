f089e60f4a55c7a3934618b4d3436d8da5e47bcd
erikvw <ew2789@gmail.com>
2016-11-01 15:56:15 -0500

    create simple home page

--- edc_visit_schedule/urls.py
+++ edc_visit_schedule/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
-from django.conf.urls import include, url
+from django.conf.urls import url
 from edc_visit_schedule.views import HomeView
 
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'', HomeView.as_view(), name='edc-visit-schedule-url'),
-    url(r'^settings/$', HomeView.as_view(), name='home_url'),
-    #url(r'', HomeView.as_view(), name='home_url'),
+
+    url(r'(?P<visit_schedule>[0-9A-Za-z_]+)/(?P<schedule>^[0-9A-Za-z_]+$)/(?P<visit_code>^[0-9]+$)/',
+        HomeView.as_view(), name='home-url'),
+    url(r'(?P<visit_schedule>[0-9A-Za-z_]+)/(?P<schedule>^[0-9A-Za-z_]+$)/', HomeView.as_view(), name='home-url'),
+    url(r'(?P<visit_schedule>[0-9A-Za-z_]+)/', HomeView.as_view(), name='home-url'),
+    url(r'', HomeView.as_view(), name='home-url'),
 ]
--- edc_visit_schedule/views.py
+++ edc_visit_schedule/views.py
@@ -1,7 +1,20 @@
-from edc_base.views.edc_base_view_mixin import EdcBaseViewMixin
+from edc_base.view_mixins import EdcBaseViewMixin
 from django.views.generic.base import TemplateView
+from edc_visit_schedule.site_visit_schedules import site_visit_schedules
+from edc_visit_schedule.exceptions import VisitScheduleError
 
 
 class HomeView(EdcBaseViewMixin, TemplateView):
 
     template_name = 'edc_visit_schedule/home.html'
+
+    def get_context_data(self, **kwargs):
+        context_data = super(HomeView, self).get_context_data(**kwargs)
+        try:
+            selected_visit_schedule = site_visit_schedules.get_visit_schedule(self.kwargs.get('visit_schedule'))
+        except VisitScheduleError:
+            selected_visit_schedule = None
+        context_data.update({
+            'visit_schedules': site_visit_schedules.registry,
+            'selected_visit_schedule': selected_visit_schedule})
+        return context_data
