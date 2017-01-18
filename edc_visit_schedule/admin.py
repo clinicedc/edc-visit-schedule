f79114a10d7b40cb5e7d7e40841ddd2b38dd6c7c
erikvw <ew2789@gmail.com>
2017-01-18 07:38:53 +0200

    add fieldset tuples

---
+++ edc_visit_schedule/admin.py
@@ -0,0 +1,6 @@
+visit_schedule_fields = ('visit_schedule_name', 'schedule_name', 'visit_code')
+
+visit_schedule_fieldset_tuple = (
+    'Visit Schedule', {
+        'classes': ('collapse',),
+        'fields': visit_schedule_fields})
