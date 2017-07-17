e59b9e82b95a08cc88a61debdcd5b66b124537ec
erikvw <ew2789@gmail.com>
2017-07-17 15:16:04 +0200

    trivial

--- edc_visit_schedule/__init__.py
+++ edc_visit_schedule/__init__.py
@@ -1,5 +1,4 @@
 from .schedule import Schedule
 from .site_visit_schedules import site_visit_schedules
-from .visit import Visit
+from .visit import Visit, Crf, Requisition, FormsCollection, Panel
 from .visit_schedule import VisitSchedule
-from .visit import Crf, Requisition
