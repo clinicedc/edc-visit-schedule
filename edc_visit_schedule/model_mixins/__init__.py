b4d94166ba6890cb9c48a35f1809ecd4728ce97a
erikvw <ew2789@gmail.com>
2017-12-17 22:33:44 +0200

    minor

--- edc_visit_schedule/model_mixins/__init__.py
+++ edc_visit_schedule/model_mixins/__init__.py
@@ -1,6 +1,6 @@
 from .base_enrollment_model_mixin import EnrollmentModelError
 from .disenrollment_model_mixin import DisenrollmentModelMixin
 from .enrollment_model_mixin import EnrollmentModelMixin
-from .visit_schedule_model_mixins import VisitScheduleFieldsModelMixin
+from .visit_schedule_model_mixins import VisitScheduleFieldsModelMixin, VisitScheduleMetaMixin
 from .visit_schedule_model_mixins import VisitScheduleMethodsModelMixin
 from .visit_schedule_model_mixins import VisitScheduleModelMixin, VisitScheduleMethodsError
