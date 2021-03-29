4b259a0f7c62f0d0f39596652be9e58379cc8151
erikvw <ew2789@gmail.com>
2021-03-29 17:23:01 -0500

    add typing hints

--- edc_visit_schedule/visit/crf.py
+++ edc_visit_schedule/visit/crf.py
@@ -1,4 +1,7 @@
+from typing import List, Type
+
 from django.apps import apps as django_apps
+from django.db import models
 
 
 class CrfLookupError(Exception):
@@ -7,8 +10,13 @@ class CrfLookupError(Exception):
 
 class Crf:
     def __init__(
-        self, show_order=None, model=None, required=None, additional=None, site_ids=None
-    ):
+        self,
+        show_order: int = None,
+        model: str = None,
+        required: bool = None,
+        additional: bool = None,
+        site_ids: List[int] = None,
+    ) -> None:
         self.additional = additional
         self.model = model
         self.required = True if required is None else required
@@ -27,14 +35,17 @@ class Crf:
     def validate(self):
         """Raises an exception if the model class lookup fails."""
         try:
-            self.model_cls
+            self.get_model_cls()
         except LookupError as e:
             raise CrfLookupError(e) from e
 
+    def get_model_cls(self):
+        return self.model_cls
+
     @property
-    def model_cls(self):
+    def model_cls(self) -> Type[models.Model]:
         return django_apps.get_model(self.model)
 
     @property
-    def verbose_name(self):
+    def verbose_name(self) -> str:
         return self.model_cls._meta.verbose_name
