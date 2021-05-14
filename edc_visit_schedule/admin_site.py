358533e6109a002fb1416ef6f684c6456a1b4ffc
erikvw <ew2789@gmail.com>
2021-05-14 16:30:06 -0500

    use edc admin site

--- edc_visit_schedule/admin_site.py
+++ edc_visit_schedule/admin_site.py
@@ -1,11 +1,7 @@
 from edc_model_admin.admin_site import EdcAdminSite
 
+from .apps import AppConfig
 
-class AdminSite(EdcAdminSite):
-    site_title = "Edc Visit Schedule"
-    site_header = "Edc Visit Schedule"
-    index_title = "Edc Visit Schedule"
-    # site_url = "/edc_visit_schedule/"
-
-
-edc_visit_schedule_admin = AdminSite(name="edc_visit_schedule_admin")
+edc_visit_schedule_admin = EdcAdminSite(
+    name="edc_visit_schedule_admin", app_label=AppConfig.name
+)
