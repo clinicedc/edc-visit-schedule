2b9a251612d8fbfaf18821efd28ba11227218fe6
erikvw <ew2789@gmail.com>
2022-08-09 13:10:32 -0500

    as tuples, add typing hints

--- edc_visit_schedule/fieldsets.py
+++ edc_visit_schedule/fieldsets.py
@@ -1,13 +1,22 @@
-visit_schedule_fields = ["visit_schedule_name", "schedule_name", "visit_code"]
+from typing import Dict, Tuple
 
-visit_schedule_fieldset_tuple = (
+visit_schedule_fields: Tuple[str, ...] = (
+    "visit_schedule_name",
+    "schedule_name",
+    "visit_code",
+)
+
+visit_schedule_fieldset_tuple: Tuple[str, Dict[str, Tuple[str, ...]]] = (
     "Visit Schedule",
     {"classes": ("collapse",), "fields": visit_schedule_fields},
 )
 
-visit_schedule_only_fields = ("visit_schedule_name", "schedule_name")
+visit_schedule_only_fields: Tuple[str, ...] = (
+    "visit_schedule_name",
+    "schedule_name",
+)
 
-visit_schedule_only_fieldset_tuple = (
+visit_schedule_only_fieldset_tuple: Tuple[str, Dict[str, Tuple[str, ...]]] = (
     "Visit Schedule",
     {"classes": ("collapse",), "fields": visit_schedule_only_fields},
 )
