b8f5fc7d055f95a17d08fd5cb358ad1303d3dd39
erikvw <ew2789@gmail.com>
2024-01-26 15:37:19 -0600

    research_protocol_config

--- visit_schedule_app/consents.py
+++ visit_schedule_app/consents.py
@@ -1,12 +1,12 @@
 from edc_consent.consent_definition import ConsentDefinition
 from edc_constants.constants import FEMALE, MALE
-from edc_protocol import Protocol
+from edc_protocol.research_protocol_config import ResearchProtocolConfig
 
 consent_v1 = ConsentDefinition(
     "visit_schedule_app.subjectconsent",
     version="1",
-    start=Protocol().study_open_datetime,
-    end=Protocol().study_close_datetime,
+    start=ResearchProtocolConfig().study_open_datetime,
+    end=ResearchProtocolConfig().study_close_datetime,
     age_min=18,
     age_is_adult=18,
     age_max=64,
