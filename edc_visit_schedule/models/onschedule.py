778169b157b2ce2e6907035f77bdbb8627682493
Jonathan Willitts <JonathanWillitts@users.noreply.github.com>
2024-02-02 23:23:17 +0000

    Bump pre-commit repos, run through Black 24.1.1
    Black changes witnessed (mainly) include:
    - addition of blank line between module docstrings and imports
    - removal of blank lines between class declarations and docstrings
    - prefer splitting assignment statements on right-hand side
    - wrap conditional expressions that span multiple lines in parens
    - put `...` in stubs on same line
    - add E701 and E704 to flake8 ignore list, see:
        - https://github.com/psf/black/blob/main/docs/guides/using_black_with_other_tools.md#e701--e704
        - https://github.com/psf/black/issues/3887 (E704)
        - https://github.com/psf/black/issues/4173 (E701)

--- edc_visit_schedule/models/onschedule.py
+++ edc_visit_schedule/models/onschedule.py
@@ -5,7 +5,6 @@ from ..model_mixins import OnScheduleModelMixin
 
 
 class OnSchedule(SiteModelMixin, OnScheduleModelMixin, BaseUuidModel):
-
     """A model used by the system. Auto-completed by subject_consent."""
 
     class Meta(OnScheduleModelMixin.Meta, BaseUuidModel.Meta):
